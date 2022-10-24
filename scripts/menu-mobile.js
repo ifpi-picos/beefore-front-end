7efea2cb2fb60c85ef012ebe1d2018f8e345305d
alanfilho184 <alan.filho@hotmail.com>
2022-10-24 13:17:33 -0300

    v0.6.4 - Finalizado menu mobile

---
+++ scripts/menu-mobile.js
@@ -0,0 +1,24 @@
+const buttonMenu = document.getElementById("button-menu")
+
+const navList = document.querySelector(".nav-list")
+
+
+if (window.screen.width <= 750) {
+    navList.setAttribute("active", "false")
+    navList.style = "display: none;"
+}
+else {
+    navList.setAttribute("active", "true")
+    navList.style = "display: flex;"
+}
+
+buttonMenu.addEventListener("click", () => {
+    if (navList.getAttribute("active") == "false") {
+        navList.style = "display: flex;"
+        navList.setAttribute("active", "true")
+    }
+    else {
+        navList.style = "display: none;"
+        navList.setAttribute("active", "false")
+    }
+})
