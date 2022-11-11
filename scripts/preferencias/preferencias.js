802c892657b7ab69ff429427144d65bdfc7cf0df
alanfilho184 <alan.filho@hotmail.com>
2022-11-11 20:41:03 -0300

    v0.10.0 - Integração das preferencias e registros

---
+++ scripts/preferencias/preferencias.js
@@ -0,0 +1,148 @@
+const apiURL = document.location.host != "beefore.netlify.app" ? "http://localhost:3001" : "https://beefore.kamiapp.com.br"
+
+if (window.localStorage.getItem("dev") == "true" || document.location.host != "127.0.0.1:5500") {
+    const res = await fetch(apiURL + "/user/", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
+
+    if (res.status == 200) {
+        const forms = document.querySelector(".forms")
+        const loading = document.getElementById("loading")
+        const response = document.querySelector(".response")
+        const responseTitle = document.getElementById("response-title")
+        const responseText = document.getElementById("response-text")
+
+        const user = await res.json()
+
+        const inputNome = document.getElementById("input-preferencias-nome")
+        const inputOcupacao = document.getElementById("input-preferencias-funcao")
+        const inputImagem = document.getElementById("input-preferencias-imagem")
+        const inputSenha = document.getElementById("input-preferencias-senha")
+        const checkComprovante = document.getElementById("comprovante")
+
+        inputNome.value = user.name
+        inputOcupacao.value = user.occupation
+        inputImagem.value = user.profileimage
+
+        if (user.telegramid) {
+            if (user.preferences.sendActionReg) {
+                checkComprovante.checked = true
+            }
+            else {
+                checkComprovante.checked = false
+            }
+        }
+        else {
+            checkComprovante.disabled = true
+        }
+
+        document.getElementById("button-confirmar").addEventListener("click", async () => {
+            const newUserInfo = {
+                modify: {
+                    name: inputNome.value != user.name ? inputNome.value : undefined,
+                    occupation: inputOcupacao.value != user.occupation ? inputOcupacao.value : undefined,
+                    profileimage: inputImagem.value != user.profileimage ? inputImagem.value : undefined,
+                    password: inputSenha.value != "" ? inputSenha.value : undefined,
+                    preferences: checkComprovante.checked != user.preferences.sendActionReg ? { sendActionReg: checkComprovante.checked } : undefined
+                }
+            }
+
+            if (!newUserInfo.modify.name && !newUserInfo.modify.occupation && !newUserInfo.modify.profileimage && !newUserInfo.modify.password && !newUserInfo.modify.preferences) {
+                return
+            }
+
+            forms.style.display = "none"
+            loading.style.display = "flex"
+
+            if (newUserInfo.modify.name && !newUserInfo.modify.name.match(/^[a-zA-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]{2,}(?: [a-zA-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+){0,8}$/g)) {
+                loading.style = "display:none"
+                response.style = "display:flex;"
+                forms.style = "display:none;"
+
+                responseTitle.innerHTML = "Nome inválido"
+                responseText.innerHTML = "Preencha corretamente o campo para realizar o cadastro"
+
+                document.getElementById("button-confirm").addEventListener("click", () => {
+                    response.style = "display:none;"
+                    forms.style = "display:block;"
+                })
+            }
+            else if (newUserInfo.modify.occupation && !newUserInfo.modify.occupation.match(/^[a-zA-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ-]{2,}(?: [a-zA-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ-]+){0,8}$/g)) {
+                loading.style = "display:none"
+                response.style = "display:flex;"
+                forms.style = "display:none;"
+
+                responseTitle.innerHTML = "Ocupação inválida"
+                responseText.innerHTML = "Preencha corretamente o campo para realizar o cadastro"
+
+                document.getElementById("button-confirm").addEventListener("click", () => {
+                    response.style = "display:none;"
+                    forms.style = "display:block;"
+                })
+            }
+            else if (newUserInfo.modify.profileimage && !newUserInfo.modify.profileimage.match(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/i)) {
+                loading.style = "display:none"
+                response.style = "display:flex;"
+                forms.style = "display:none;"
+
+                responseTitle.innerHTML = "URL de imagem inválido"
+                responseText.innerHTML = "Este não é um URL válido para uma imagem"
+
+                document.getElementById("button-confirm").addEventListener("click", () => {
+                    response.style = "display:none;"
+                    forms.style = "display:block;"
+                })
+            }
+            else if (newUserInfo.modify.password && newUserInfo.modify.password.length < 8) {
+                loading.style = "display:none"
+                response.style = "display:flex;"
+                forms.style = "display:none;"
+
+                responseTitle.innerHTML = "Senha inválida"
+                responseText.innerHTML = "Sua senha precisa ter no mínimo 8 caracteres"
+
+                document.getElementById("button-confirm").addEventListener("click", () => {
+                    response.style = "display:none;"
+                    forms.style = "display:block;"
+                })
+            }
+            else if (newUserInfo.modify.password && newUserInfo.modify.password != document.getElementById("input-preferencias-confirmar-senha").value) {
+                loading.style = "display:none"
+                response.style = "display:flex;"
+                forms.style = "display:none;"
+
+                responseTitle.innerHTML = "Senha inválida"
+                responseText.innerHTML = "A senha não coincide com a confirmação"
+
+                document.getElementById("button-confirm").addEventListener("click", () => {
+                    response.style = "display:none;"
+                    forms.style = "display:block;"
+                })
+            }
+            else {
+                const res = await fetch(apiURL + "/user/", {
+                    method: "PATCH",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': window.localStorage.getItem('token')
+                    },
+                    body: JSON.stringify(newUserInfo)
+                })
+
+                if (res.status == 200) {
+                    document.location.reload()
+                }
+                else {
+                    console.log(res)
+                }
+            }
+        })
+
+    }
+}
---
+++ scripts/registros/registros.js
@@ -0,0 +1,54 @@
+const apiURL = document.location.host != "beefore.netlify.app" ? "http://localhost:3001" : "https://beefore.kamiapp.com.br"
+
+if (window.localStorage.getItem("dev") == "true" || document.location.host != "127.0.0.1:5500") {
+    const res = await fetch(apiURL + "/relatory/", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
+
+    if (res.status == 200) {
+        const data = await res.json()
+
+        const boxRegistro = document.querySelector(".box-registro")
+        Object.keys(data.actions).forEach(date => {
+            const actions = Object.entries(data.actions[date])
+            date = date.split("/")
+            const box = document.createElement("div")
+            box.classList.add("box")
+
+            const dataRegistro = document.createElement("div")
+            dataRegistro.classList.add("data-registro")
+
+            dataRegistro.innerHTML = `<h2>Data</h2>
+            <h2 id="dia">${date[0]}</h2>
+            <h2 id="mes">${date[1]}</h2>
+            <h2 id="ano">${date[2]}</h2>`
+
+            const linhaVertical = document.createElement("div")
+            linhaVertical.id = "linha-vertical"
+
+            const movimento = document.createElement("div")
+            movimento.classList.add("movimento")
+            actions.forEach(action => {
+                const move = document.createElement("h2")
+                const time = luxon.DateTime.fromFormat(action[0], "HH:mm:ss").setZone("America/Fortaleza").toFormat("HH:mm a")
+                move.innerText = `${action[1]} - ${time}`
+                movimento.appendChild(move)
+            })
+
+            box.appendChild(dataRegistro)
+            box.appendChild(linhaVertical)
+            box.appendChild(movimento)
+            boxRegistro.appendChild(box)
+
+        })
+
+    }
+    else if (res.status == 404) {
+
+    }
+}
